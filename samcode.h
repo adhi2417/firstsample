73280f765f3914b67010bc374f85ffe4f81a1a6d
Adhithya Alagappan <161410897+adhi2417@users.noreply.github.com>
2025-03-04 23:16:21 +0530

    Update samcode.h
    retry

--- samcode.h
+++ samcode.h
@@ -27,13 +27,4 @@ class MergeSortTracking {
         System.out.println("After iteration " + iteration++);
         System.out.println(Arrays.toString(arr).replaceAll("[\[\],]", ""));
     }
-    
-    public static void mergeSort(int[] arr, int left, int right) {
-        if (left < right) {
-            int mid = (left + right) / 2;
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid + 1, right);
-            merge(arr, left, mid, right);
-        }
-    }
- 
+
